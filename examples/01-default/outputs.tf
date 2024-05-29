9ba69b72d1b775b2b939f01fc9e85a719458c11b
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-05-29 10:49:31 +0200

    init

---
+++ examples/01-default/outputs.tf
@@ -0,0 +1,36 @@
+#Repositories
+output "repos" {
+  value = module.github_bootstrap.repos
+}
+
+#Variables
+/* output "variables" {
+  value = module.github_bootstrap.variables
+}
+output "flattened_variables" {
+  value = module.github_bootstrap.flattened_variables
+} */
+
+#Secrets
+/* output "secrets" {
+  value = module.github_bootstrap.secrets
+}
+output "flattened_secrets" {
+  value = module.github_bootstrap.flattened_secrets
+} */
+
+#Labels
+/* output "labels" {
+  value = module.github_bootstrap.labels
+}
+output "flattened_labels" {
+  value = module.github_bootstrap.flattened_labels
+} */
+
+#Examples
+/* output "examples" {
+  value = module.github_bootstrap.examples
+} */
+/* output "examples_section" {
+  value = module.github_bootstrap.examples_section
+} */
